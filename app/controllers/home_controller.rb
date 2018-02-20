a4cba23e7d743646d3054348d3653572f6e3e1e5
admin <isalechat@yahoo.com>
2018-02-20 18:34:30 +0100

    Commit Home Page

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,12 @@
+class HomeController < ApplicationController
+  def index
+  end
+  def checkr
+  end
+
+  def flynn
+  end
+
+  def sparks
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  get 'home/checkr'
+
+  get 'home/flynn'
+
+  get 'home/sparks'
+
   root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
